1e9f734004c10aff19f45842286fba380f7b1122
Andito Rizkyka <rizkykandito@gmail.com>
2025-05-18 17:26:53 +0700

    Auth UI done

--- money_mate_website/frontend/money_mate_website_fe/lib/main.dart
+++ money_mate_website/frontend/money_mate_website_fe/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      child: GetMaterialApp(
+        title: "Application",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+      ),
     ),
   );
 }
